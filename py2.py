7a21215a538e1ef8d5b8c3779001cc638c8fec14
rshah-sp <rshah@sheridanpoint.org>
2017-07-11 01:05:04 -0500

    Adding Excercise 1 - 3

---
+++ py2.py
@@ -0,0 +1,6 @@
+# This is a Comment
+# Everything after a # is ignored by Python.
+print("I could do this") #putting a comment after!
+#I could also do this:
+#print("DON"T PRINT THIS!")
+print("This, however, will print")
